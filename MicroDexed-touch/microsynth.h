3d73409bdbd286a6227dc4ee07783e63ef03edbf
localhero <davidpub@free.fr>
2023-02-06 21:41:51 +0100

    Refactoring to separate Braids and Microsynth

--- MicroDexed-touch/microsynth.h
+++ MicroDexed-touch/microsynth.h
@@ -1,8 +1,10 @@
-#ifndef microsynth_h_
-#define microsynth_h_
+#ifndef _MICROSYNTH_H_
+#define _MICROSYNTH_h_
 
 #include "Arduino.h"
 
+void update_microsynth_params();
 void microsynth_update_all_settings(uint8_t instance_id);
-void braids_update_all_settings();
+void microsynth_update_single_setting(uint8_t instance_id);
+
 #endif
