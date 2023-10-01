314774cb134b79862c06448c866adb2c553b376a
dronus <paul.geisler@web.de>
2023-10-01 12:53:58 +0200

    params: add uint32_t type, rename descriptor fields to _desc

--- MicroDexed-touch/param.h
+++ MicroDexed-touch/param.h
@@ -5,9 +5,9 @@
 class Descriptor {
 public:
   const enum Type {
-    P_END=0, P_UINT8_T, P_UINT16_T, P_INT32_T, P_FLOAT, P_TYPE_COUNT
+    P_END=0, P_UINT8_T, P_UINT16_T, P_UINT32_T, P_INT32_T, P_FLOAT, P_TYPE_COUNT
   } type;
-  const uint8_t sizes[P_TYPE_COUNT]={0,sizeof(uint8_t),sizeof(uint16_t),sizeof(int32_t),sizeof(float)};
+  // const uint8_t sizes[P_TYPE_COUNT]={0,sizeof(uint8_t),sizeof(uint16_t),sizeof(uint32_t),sizeof(int32_t),sizeof(float)};
 
   const uint8_t count;
   const char* name;
@@ -33,6 +33,7 @@ public:
     char* ptr = ((char*)this) + sizeof(Param);
     if(desc->type == Descriptor::P_UINT8_T) return *((uint8_t*)ptr);
     if(desc->type == Descriptor::P_UINT16_T) return *((uint16_t*)ptr);
+    if(desc->type == Descriptor::P_UINT32_T) return *((uint32_t*)ptr);
     if(desc->type == Descriptor::P_INT32_T) return *((int32_t*)ptr);
     if(desc->type == Descriptor::P_FLOAT) return *((float*)ptr);
     return 0;
@@ -41,7 +42,8 @@ public:
     char* ptr = ((char*)this) + sizeof(Param);
     if(desc->type == Descriptor::P_UINT8_T) *((uint8_t*)ptr) = round(value);
     if(desc->type == Descriptor::P_UINT16_T) *((uint16_t*)ptr) = round(value);
-    if(desc->type == Descriptor::P_INT32_T) *((uint32_t*)ptr) = round(value);
+    if(desc->type == Descriptor::P_UINT32_T) *((uint32_t*)ptr) = round(value);
+    if(desc->type == Descriptor::P_INT32_T) *((int32_t*)ptr) = round(value);
     if(desc->type == Descriptor::P_FLOAT) *((float*)ptr) = value;
   };
   void check() {
@@ -52,22 +54,25 @@ public:
   };
 } __attribute__ ((aligned (4)));
 
-#define P_bool(name,min,max,def) \
-  Param name##_meta{Descriptor::P_UINT8_T, 1, #name, min, max, def}; uint8_t name = def
+#define P_bool(name,def) \
+  Param name##_desc{Descriptor::P_UINT8_T, 1, #name, def}; uint8_t name = def
 
 #define P_uint8_t(name,min,max,def) \
-  Param name##_meta{Descriptor::P_UINT8_T, 1, #name, min, max, def}; uint8_t name = def
+  Param name##_desc{Descriptor::P_UINT8_T, 1, #name, min, max, def}; uint8_t name = def
 
 #define P_uint16_t(name,min,max,def) \
-  Param name##_meta{Descriptor::P_UINT16_T, 1, #name,min,max,def}; uint16_t name = def
+  Param name##_desc{Descriptor::P_UINT16_T, 1, #name,min,max,def}; uint16_t name = def
 
 #define P_int32_t(name,min,max,def) \
-  Param name##_meta{Descriptor::P_INT32_T, 1, #name,min,max,def}; int32_t name = def
+  Param name##_desc{Descriptor::P_INT32_T, 1, #name,min,max,def}; int32_t name = def
+
+#define P_uint32_t(name,min,max,def) \
+  Param name##_desc{Descriptor::P_UINT32_T, 1, #name,min,max,def}; int32_t name = def
 
 #define P_float(name,min,max,def) \
-  Param name##_meta{Descriptor::P_FLOAT, 1, #name,min,max,def}; float name = def
+  Param name##_desc{Descriptor::P_FLOAT, 1, #name,min,max,def}; float name = def
 
-#define P_end Param _p_end{Descriptor::P_END};
+#define P_end Param _p_end_desc{Descriptor::P_END};
 
 class Params {
 public:
