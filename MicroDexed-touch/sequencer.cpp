b0059b46aa4c102993e7c6ad991394a171e59e23
Damian Weber <damian.weber@axiamo.com>
2023-11-24 10:37:32 +0100

    noice

--- MicroDexed-touch/sequencer.cpp
+++ MicroDexed-touch/sequencer.cpp
@@ -81,86 +81,115 @@ extern multisample_s msp[NUM_MULTISAMPLES];
 #include "TeensyTimerTool.h"
 
 struct MidiEvent {
-    midi::MidiType event;
-    uint8_t note_in;
-    uint8_t note_in_velocity;
+  unsigned long time;
+  midi::MidiType event;
+  uint8_t note_in;
+  uint8_t note_in_velocity;
 };
 
-std::map<unsigned long, MidiEvent> midiEvents;
+static constexpr int EVENTS_SIZE = 50;
+MidiEvent midiEvents[EVENTS_SIZE] = { 0, midi::InvalidType, 0, 0 };
 elapsedMillis patternTimer;
-unsigned int currentIndex = 0;
+
+unsigned int playIndex = 0;
+unsigned int eventsSize = 0;
+
 TeensyTimerTool::OneShotTimer liveTimer;
 
 std::string getName(midi::MidiType event) {
   switch(event) {
   case midi::NoteOn:
-    return "NoteOn...";
+    return "NoteOn";
   case midi::NoteOff:
-    return "NoteOff...";    
+    return "NoteOff";
+  case midi::InvalidType:
+    return "Invalid";
   default:
     return "None";
+  }
+}
+
+void printEvent(unsigned int i) {
+  MidiEvent e = midiEvents[i];
+  Serial.printf("[%i]: %i, %s, %i, %i\n", i, e.time, getName(e.event).c_str(), e.note_in, e.note_in_velocity);
+}
+
+void printEvents() {
+  Serial.printf("--- %i events:\n", eventsSize);
+  for(unsigned int i = 0; i < eventsSize; i++) {
+    printEvent(i);
   }
 }
 
 void addEvent(midi::MidiType event, uint8_t note, uint8_t velocity) {
   if(seq.running) {
     unsigned long now = patternTimer;
-    MidiEvent e = { event, note, velocity };
-    if(midiEvents.empty() && event == midi::NoteOff) {
-      return;
+    MidiEvent e = { now, event, note, velocity };
+
+    bool clearAll = false;
+    clearAll |= note == 49;
+    if(eventsSize > 0) {
+      clearAll |= now < midiEvents[eventsSize - 1].time;
     }
-    if(midiEvents.size()) {
-      if(note == 44) {
-        midiEvents.clear();
-        Serial.printf("clear map\n");
+    if(clearAll) {
+      memset(midiEvents, 0, EVENTS_SIZE * sizeof(MidiEvent));
+      Serial.printf("clear map\n");
+      eventsSize = 0;
+      if(event == midi::NoteOff) {
         return;
       }
-      /*if(now < std::prev(midiEvents.end())->first) {
-        midiEvents.clear();
-        Serial.printf("clear map\n");
-      }*/
     }
-    midiEvents.insert(std::pair<unsigned long, MidiEvent>(now, e));
-    Serial.printf("[%i] added event: %s, %i, %i. now has %i events\n", now, getName(event).c_str(), note, velocity, midiEvents.size());
+    if(eventsSize == 0 && event == midi::NoteOff) {
+      return;
+    }
+    midiEvents[eventsSize] = e;
+    eventsSize++;
+    printEvents();
+    
   } else {
-    Serial.printf("ignoring event since not running...\n");
+    //Serial.printf("ignoring event since not running...\n");
   }
 }
 
 void playNextEvent() {
-  currentIndex++;
-  if(midiEvents.size() > currentIndex) {
-    unsigned long now = patternTimer;
-    
-    auto iter = midiEvents.begin();
-    for(unsigned int i = 0; i < currentIndex; i++) {
-      iter++;
-    }
-    Serial.printf("[%i] %s\n", now, getName(iter->second.event).c_str());
-    switch(iter->second.event) {
+  
+  if(eventsSize > playIndex) {
+    Serial.printf("PLAY: ");
+    printEvent(playIndex);
+    switch(midiEvents[playIndex].event) {
     case midi::NoteOn:
-      handleNoteOn(16, iter->second.note_in, iter->second.note_in_velocity, 0);
+      handleNoteOn(16, midiEvents[playIndex].note_in, midiEvents[playIndex].note_in_velocity, 0);
       break;
     
     case midi::NoteOff:
-      handleNoteOff(16, iter->second.note_in, iter->second.note_in_velocity, 0);
+      handleNoteOff(16, midiEvents[playIndex].note_in, midiEvents[playIndex].note_in_velocity, 0);
       break;
     
     default:
       break;
     }
-
-    liveTimer.trigger((iter->first - now) * 1000);
+    playIndex++;
+    unsigned long timeToNextEvent = midiEvents[playIndex].time - midiEvents[playIndex - 1].time;
+    if(timeToNextEvent >= 0) {
+      if(timeToNextEvent > 0) {
+        liveTimer.trigger(timeToNextEvent * 1000);
+      } else {
+        liveTimer.trigger(10);
+      }
+    } else {
+      Serial.printf("NOPE\n");
+    }
   }
   
 }
 
 void handlePatternBegin(void) {
+  printEvents();
   patternTimer = 0;
-  if(midiEvents.size()) {
-    currentIndex = 0;
+  if(eventsSize > 0) {
+    playIndex = 0;
     liveTimer.begin(playNextEvent);
-    liveTimer.trigger(midiEvents.begin()->first * 1000);
+    liveTimer.trigger(midiEvents[0].time * 1000);
   }
 }
 
