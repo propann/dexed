5f2a53261e066d7c932f006b8bfed49a7641fe64
Damian Weber <dweb@gmx.net>
2024-08-02 08:49:15 +0200

    track setup now only shows existing MDT instruments in selection

--- MicroDexed-touch/ui_livesequencer.cpp
+++ MicroDexed-touch/ui_livesequencer.cpp
@@ -141,6 +141,10 @@ FLASHMEM void UI_LiveSequencer::init(void) {
   [ this, applyTrackInstrument ] (auto *b, auto *v) { // drawHandler
     char name[10];
     char sub[10];
+    // MDT has instruments 0-6 / MIDI has instuments 0-15
+    const uint8_t maxInstrument = (selectedTrackDevice == LiveSequencer::DEVICE_INTERNAL) ? 6 : 15;
+    static_cast<EditableValueRange<uint8_t>*>(v)->changeRange(0, maxInstrument); 
+    
     liveSeq.getInstrumentName(selectedTrackDevice, v->getValue(), name, sub);
     b->draw(name, sub, TouchButton::BUTTON_ACTIVE);
     applyTrackInstrument->drawNow();
